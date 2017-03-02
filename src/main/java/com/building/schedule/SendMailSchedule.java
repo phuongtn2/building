856d463b74a75679b6117f0358307d083f11e740
phuongtn1 <phuongtn1@aoi-sys.vn>
2017-03-02 10:27:52 +0700

    Ref# add send mail

---
+++ src/main/java/com/building/schedule/SendMailSchedule.java
@@ -0,0 +1,10 @@
+package com.building.schedule;
+
+import java.util.List;
+
+/**
+ * Created by PhuongTN1 on 11/4/2016.
+ */
+public interface SendMailSchedule {
+    public abstract void sendMail();
+}
---
+++ src/main/java/com/building/schedule/impl/SendMailScheduleImpl.java
@@ -0,0 +1,18 @@
+package com.building.schedule.impl;
+
+import com.building.schedule.SendMailSchedule;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by PhuongTN1 on 11/4/2016.
+ */
+@Component("sendMail")
+public class SendMailScheduleImpl implements SendMailSchedule {
+
+    @Override
+    public void sendMail() {
+
+    }
+}
